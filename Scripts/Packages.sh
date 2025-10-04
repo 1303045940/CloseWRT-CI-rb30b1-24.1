2ae2c6cab3c1d360d5c9ad4ed26aafaa90676262
1303045940 <1332338040@qq.com>
2025-10-05 00:03:23 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -83,6 +83,8 @@ UPDATE_PACKAGE "luci-app-quickstart" "shidahuilang/openwrt-package" "Immortalwrt
 #UPDATE_PACKAGE "luci-app-quickstart" "shidahuilang/openwrt-package" "Immortalwrt" "pkg"
 #UPDATE_PACKAGE "quickstart" "master-yun-yun/package-istore" "Immortalwrt" "pkg"
 #UPDATE_PACKAGE "luci-app-quickstart" "shidahuilang/openwrt-package" "Immortalwrt" "pkg"
+
+UPDATE_PACKAGE "quickstart" "kiddin9/kwrt-packages" "Immortalwrt" "pkg"
 UPDATE_PACKAGE "luci-app-quickstart" "kiddin9/kwrt-packages" "Immortalwrt" "pkg"
 UPDATE_PACKAGE "luci-app-adguardhome" "shidahuilang/openwrt-package" "Immortalwrt" "pkg"
 
