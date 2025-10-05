1370b41c2d4bd69518e2203a4895016019372f88
1303045940 <1332338040@qq.com>
2025-10-06 01:08:07 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -70,18 +70,57 @@ fi
 # 定义你要插入的shell片段内容
 insert_content='if [ ! -f /etc/npc-init.flag ]; then
     WAN_IF=$(uci get network.wan.ifname 2>/dev/null || echo "wan")
-    WAN_MAC=$(cat /sys/class/net/$WAN_IF/address)
-    #VKEY=$(echo -n "$WAN_MAC" | md5sum | awk '\''{print $1}'\'')
-	VKEY=${WAN_MAC}
-  #  uci set npc.@npc[0].server_addr="192.168.5.100"
-	uci set npc.@npc[0].server_addr="nps.5251314.xyz"
+    PERSIST_MAC_FILE="/etc/npc_wan_mac"
+    # 优先使用已持久化的 WAN MAC（如果存在）
+    if [ -f "$PERSIST_MAC_FILE" ]; then
+        WAN_MAC=$(cat "$PERSIST_MAC_FILE")
+    else
+        # 否则尝试从内核获取并持久化，以便后续刷机保留同一个值
+        if [ -f "/sys/class/net/$WAN_IF/address" ]; then
+            WAN_MAC=$(cat /sys/class/net/$WAN_IF/address)
+            if [ -n "$WAN_MAC" ]; then
+                echo "$WAN_MAC" > "$PERSIST_MAC_FILE"
+            fi
+        fi
+    fi
+    # 直接使用 WAN_MAC 作为 VKEY（如需 md5 可在此处做转换）
+    VKEY=${WAN_MAC}
+    uci set npc.@npc[0].server_addr="nps.5251314.xyz"
     uci set npc.@npc[0].vkey="$VKEY"
     uci set npc.@npc[0].compress="1"
     uci set npc.@npc[0].crypt="1"
     uci set npc.@npc[0].enable="1"
+	uci set npc.@npc[0].server_port="8024"
+    uci set npc.@npc[0].protocol="tcp"
     uci commit npc
+	sed -i 's|conf_Path="/tmp/etc/npc.conf"|conf_Path="/etc/npc.conf"|g' /etc/init.d/npc
+	
+	# 使用 sed 插入内容
+	touch /etc/npc.conf
+	sed -i "1i[common]" /etc/npc.conf && \
+	sed -i "2iserver_addr=nps.5251314.xyz:8024" /etc/npc.conf && \
+	sed -i "3iconn_type=tcp" /etc/npc.conf && \
+	sed -i "4ivkey=${VKEY}" /etc/npc.conf && \
+	sed -i "5iauto_reconnection=true" /etc/npc.conf && \
+	sed -i "6icompress=true" /etc/npc.conf && \
+	sed -i "7icrypt=true" /etc/npc.conf
 
+# 创建文件并写入所有内容
+#cat << EOF > /etc/npc.conf
+#[common]
+#server_addr=nps.5251314.xyz:8024
+#conn_type=tcp
+#vkey=${VKEY}
+#auto_reconnection=true
+#compress=true
+#crypt=true
+#EOF
+	# 验证修改
+echo "文件内容如下："
+cat /etc/npc.conf
     touch /etc/npc-init.flag
+    /etc/init.d/npc enable   # 设置开机自启
+    /etc/init.d/npc restart
 fi
 '
 
