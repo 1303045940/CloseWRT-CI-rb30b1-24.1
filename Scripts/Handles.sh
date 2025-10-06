36aca620dc2dbdc9d9141f54f812fa750d2efb9b
1303045940 <1332338040@qq.com>
2025-10-06 09:29:57 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -66,28 +66,15 @@ fi
 #	cd $PKG_PATH && echo "tailscale has been fixed!"
 #fi
 
-#修复Rust编译失败Add commentMore actions
-#修复状态灯
-#LED_FILE="./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts"
-#DTS_FILE="./files/ipq6000-re-ss-01.dts"
-#if [ -f "$LED_FILE" ]; then
-#	echo " "
-# 
-#    cp -f "$DTS_FILE" "$LED_FILE"
-#
-#	echo "状态灯修复完成!"
-#else
-#    echo "状态灯修复失败" 
-#fi
+#修复Rust编译失败
+RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
+if [ -f "$RUST_FILE" ]; then
+	echo " "
 
-# Set Rust build arg llvm.download-ci-llvm to false.#修复Rust编译失败Add commentMore actions
-RUST_MAKEFILE="feeds/packages/lang/rust/Makefile"
-if [[ -f "${RUST_MAKEFILE}" ]]; then
-  sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' "${RUST_MAKEFILE}"
-else
-  echo "File ${RUST_MAKEFILE} does not exist." >&2
+	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
+
+	cd $PKG_PATH && echo "rust has been fixed!"
 fi
-sed -i 's/download-ci-llvm = true/download-ci-llvm = "if-unchanged"/' feeds/packages/lang/rust/files/config.toml
 
 #预置OpenClash内核和数据
 if [ -d *"openclash"* ]; then
